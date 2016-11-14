fc35af5dbc8b15446995ad21db979a35b18195aa
DecaHub <dan@decahub.com>
2016-11-14 06:34:51 -0500

    Add endpoint to update asset

--- app/queries.js
+++ app/queries.js
@@ -71,10 +71,36 @@ let createAsset = (req, res, next) => {
 		})
 };
 
+let updateAsset = (req, res, next) => {
+	
+	console.log(`updateAsset`);
+	
+	// console.log(req.body);
+	
+	let data = req.body;
+	let id = req.params.id;
+	
+	// console.log(data);
+	
+	console.log(`UPDATE assets SET media=\'${data.media}\'::text, type=\'${data.type}\'::text, title=\'${data.title}\'::text, author=\'${data.author}\', source=\'${data.source}\'::text, category=\'${data.category}\' WHERE id = ${id}`);
+	
+	db.none(`UPDATE assets SET media=\'${data.media}\'::text, type=\'${data.type}\'::text, title=\'${data.title}\'::text, author=\'${data.author}\', source=\'${data.source}\'::text, category=\'${data.category}\' WHERE id = ${id}`)
+		.then(() => {
+			res.status(200)
+				.json({
+					status: "success",
+					message: "Updated asset"
+				});
+		})
+		.catch((error) => {
+			return next(error);
+		})
+};
+
 module.exports = {
 	getAllAssets: getAllAssets,
 	getSingleAsset: getSingleAsset,
 	createAsset: createAsset,
-	// updateAsset: updateAsset,
+	updateAsset: updateAsset
 	// removeAsset: removeAsset
 };
