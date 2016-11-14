fa94966f73b193fb8a3f1c2846a55da5caa7afe1
DecaHub <dan@decahub.com>
2016-11-14 06:32:59 -0500

    Use cors package to address CORS issues

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,11 +13,13 @@ const db = require('./queries');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+// });
+
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send("Hello from server.js");
@@ -26,7 +29,7 @@ app.get('/', (req, res) => {
 app.get('/api/assets', db.getAllAssets);
 app.get('/api/assets/:id', db.getSingleAsset);
 app.post('/api/assets', db.createAsset);
-// router.put('/api/assets/:id', db.updateAsset);
+app.put('/api/assets/:id', db.updateAsset);
 // router.delete('/api/assets/:id', db.removeAsset);
 
 app.listen(8000, () => {
