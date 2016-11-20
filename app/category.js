8e06af3af2ebf1ce34a7782175c271b09b80bb55
DecaHub <dan@decahub.com>
2016-11-19 20:51:22 -0500

    Add method to store categories and subcategories

--- app/category.js
+++ app/category.js
@@ -1,15 +1,47 @@
 "use strict";
 
-const fs = require('fs');
+require('dotenv').config();
 
-let topics_data = null;
+let queryUtil = require("./queries.util");
+const fs = require("fs");
 
-fs.readFile('topics.json', 'utf-8', (error,  data) => {
+const conn = require("./db");
+
+const db = conn.connect2DB(process.env.PGDB_LIB);
+
+let category_data = null;
+
+fs.readFile("app/topics.json", "utf-8", (error,  data) => {
 	if (error) {
 		console.log(error);
 		throw error;
 	} else {
-		topics_data = JSON.parse(data);
-		console.log(topics_data);
+		category_data = JSON.parse(data);
+		storeCategory();
 	}
 });
+
+let storeCategory = () => {
+	
+	for (let i = 0; i < category_data.length; ++i) {
+		
+		if (category_data[i].category) {
+			
+			if (!category_data[i].subcategory) {
+				category_data[i].subcategory = [];
+			}
+				
+			category_data[i].subcategory = queryUtil.jsArrayToPSQLArray(category_data[i].subcategory);
+			
+			console.log(`INSERT INTO ${process.env.PGTB_CAT} VALUES ('${category_data[i].category}', '${category_data[i].subcategory}'`);
+			
+			db.query(`INSERT INTO ${process.env.PGTB_CAT} (category, subcategory) VALUES ('${category_data[i].category}', '${category_data[i].subcategory}');`)
+			
+		}
+	}
+};
+
+
+
+
+// db.query(`INSERT INTO ${process.env.PGTB_CAT} `)
