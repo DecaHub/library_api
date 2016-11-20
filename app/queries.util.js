39389521221d47e9b52b8a30d81559e322733400
DecaHub <dan@decahub.com>
2016-11-19 20:49:34 -0500

    Account for empty, null and undefined array

--- app/queries.util.js
+++ app/queries.util.js
@@ -7,40 +7,47 @@ let postgresStringifyListProperty = (prop) => {
 	const LAST_ELEMENT = array.length - OFFSET;
 	const FIRST_ELEMENT = 0;
 	const ONE_ELEMENT = 1;
+	const EMPTY = 0;
 	
 	const SINGLE_INC = 1;
 	
 	let postgresStringArray = "";
 	
-	for (let element = FIRST_ELEMENT;
-	     element < array.length; element += SINGLE_INC) {
-		
-		if (array.length === ONE_ELEMENT) {
+	if (prop == null || prop == undefined) {
+		return null;
+	} else if (prop.length === 0) {
+		postgresStringArray += `{}`;
+	} else {
+		for (let element = FIRST_ELEMENT;
+		     element < array.length; element += SINGLE_INC) {
 			
-			// There is only one element in the array
-			
-			postgresStringArray += `{"${array[element]}"}`;
-			
-		} else if (element === FIRST_ELEMENT) {
-			
-			// We are looking at the first element
-			
-			postgresStringArray += `{"${array[element]}", `;
-			
-		} else if (element === LAST_ELEMENT) {
-			
-			// We are looking at the last element
-			
-			postgresStringArray += `"${array[element]}"}`;
-			
-		} else {
-			
-			// We are looking at an in-between element
-			
-			postgresStringArray += `"${array[element]}", `;
+			if (array.length === ONE_ELEMENT) {
+				
+				// There is only one element in the array
+				
+				postgresStringArray += `{"${array[element]}"}`;
+				
+			} else if (element === FIRST_ELEMENT) {
+				
+				// We are looking at the first element
+				
+				postgresStringArray += `{"${array[element]}", `;
+				
+			} else if (element === LAST_ELEMENT) {
+				
+				// We are looking at the last element
+				
+				postgresStringArray += `"${array[element]}"}`;
+				
+			} else {
+				
+				// We are looking at an in-between element
+				
+				postgresStringArray += `"${array[element]}", `;
+				
+			}
 			
 		}
-		
 	}
 	
 	return postgresStringArray;
