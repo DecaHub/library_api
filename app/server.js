e40eb2c59c3c64f239cc30679cc286648ddfea3d
DecaHub <dan@decahub.com>
2016-11-19 20:42:06 -0500

    Update module name for asset database

--- app/server.js
+++ app/server.js
@@ -8,16 +8,11 @@ const cors = require('cors');
 
 const app = express();
 
-const db = require('./queries');
+const db = require('./assets');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// app.use(function(req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	next();
-// });
 
 app.use(cors());
 
